0b6c165ab891ff9bc21af70e58604fc043e964a3
Alexander Dolidze <alexander.dolidze@gmail.com>
2024-04-15 04:01:33 +0400

    create security group

--- main.tf
+++ main.tf
@@ -19,8 +19,9 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+
   vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
@@ -29,10 +30,9 @@ resource "aws_instance" "blog" {
 }
 
 resource "aws_security_group" "blog" {
-  name = "blog"
-  tags = {
-    Terraform = "true"
-  }
+  name        = "blog"
+  description = "Allow http and https in. Allow everything out."
+
   vpc_id = data.aws_vpc.default.id
 }
 
@@ -42,25 +42,16 @@ resource "aws_security_group_rule" "blog_http_in" {
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+
   security_group_id = aws_security_group.blog.id
 }
-
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
-}
-
 
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
-  protocol    = "-1"
+  protocol    = -1
   cidr_blocks = ["0.0.0.0/0"]
+
   security_group_id = aws_security_group.blog.id
 }
